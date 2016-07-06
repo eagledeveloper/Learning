c6669bb432b01d99c2b01d6faa25b7e02b00d72f
eagledeveloper <decarlivale@gmail.com>
2016-07-06 22:46:33 +0200

    lesson 2

---
+++ learning/src/ch/gmtech/learning/lesson2/Course.java
@@ -0,0 +1,17 @@
+package ch.gmtech.learning.lesson2;
+
+public class Course {
+	
+	public void getName() {
+		
+	}
+	
+	public void getNumber() {
+		
+	}
+	
+	public void getDescription() {
+		
+	}
+
+}
---
+++ learning/src/ch/gmtech/learning/lesson2/Seminar.java
@@ -0,0 +1,25 @@
+package ch.gmtech.learning.lesson2;
+
+public class Seminar {
+	
+	public void getName() {
+		
+	}
+	
+	public void getDescription() {
+		
+	}
+	
+	public void getLocation() {
+		
+	}
+	
+	public void getSeatsLeft() {
+		
+	}
+	
+	public void getStudentList() {
+		
+	}
+
+}
