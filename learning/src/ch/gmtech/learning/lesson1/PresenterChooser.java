8a798291a420a043249159f5e1665a66fb6ca782
valentino <valentino.decarli@gmtech.ch>
2016-07-19 10:58:05 +0200

    Presenter chooser

--- learning/src/ch/gmtech/learning/lesson1/PresenterChooser.java
+++ learning/src/ch/gmtech/learning/lesson1/PresenterChooser.java
@@ -1,5 +1,9 @@
 package ch.gmtech.learning.lesson1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,40 +14,38 @@ public class PresenterChooser {
 	private static List<String> presenters = Arrays.asList("Gennaro", "Dario", "Mattia", "Stefano", "Valentino",
 			"Vacca", "Enrico", "Manlio", "Alessandro", "Chicco", "Pino", "Matteo", "Massi", "Gabri");
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-//		String pathRegistered = "";
+		String pathRegistered = "/Data/Reporting/Valentino/Learning/registered.txt";
+		String pathExcluded = "/Data/Reporting/Valentino/Learning/excluded.txt";
 		
-//		Files.readAllLines(pathRegistered, new Charse);
-		
-		List<String> newPresenters = new ArrayList<String>();
-		newPresenters.addAll(presenters);
+		List<String> registered = list(pathRegistered);
+		List<String> excluded = list(pathExcluded);
 		
 		System.out.println("Total number of possible presenters: " + presenters.size());
+		System.out.println("Presenters to remove: " + excluded);
 		
-		if(args.length > 0) {
-			
-			List<String> presentersToRemove = new ArrayList<String>();
-			
-			for(int i = 0; i < args.length; i++) {
-				
-				presentersToRemove.add(args[i]);
-				
-			}
-			
-			System.out.println("Presenters to remove: " + presentersToRemove);
-			
-			newPresenters.removeAll(presentersToRemove);
-			
-		}
+		registered.removeAll(excluded);
 		
-		System.out.println("Papable presenters number: " + newPresenters.size());
+		System.out.println("Papable presenters number: " + registered.size());
 		
 		Random random = new Random(System.currentTimeMillis());
-		int index = random.nextInt(newPresenters.size());
+		int index = random.nextInt(registered.size());
 		
-		System.out.println("Presenter selected: " + newPresenters.get(index));
+		System.out.println("Presenter selected: " + registered.get(index));
 		
+	}
+	
+	public static List<String> list(String path) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+
+		String line = null;
+		List<String> returnList = new ArrayList<String>();
+		while ((line = br.readLine()) != null) {
+			returnList.add(line);
+		}
+		br.close();
+		return returnList;
 	}
 
 }
