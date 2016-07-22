b9c3d13baafacce5e34b5ebc5e1ac145e4cc66ab
valentino <valentino.decarli@gmtech.ch>
2016-07-22 13:53:22 +0200

    Solid suite of tests

--- learning/src/ch/gmtech/learning/lesson4/test/CustomerTest.java
+++ learning/src/ch/gmtech/learning/lesson4/test/CustomerTest.java
@@ -12,23 +12,26 @@ public class CustomerTest {
 	
 	@Test
 	public void testCustomer() {
-		Customer customer = new Customer("Stefano Frocio");
-		Movie movie1 = new Movie("Ritorno al futuro", 0);
+		Customer customer = new Customer("Ugo Campione");
+		Movie movie1 = new Movie("Ritorno al futuro", Movie.REGULAR);
 		Rental rental1 = new Rental(movie1 , 5);
-		Movie movie2 = new Movie("StarWars Episode 8", 1);
+		Movie movie2 = new Movie("StarWars Episode 8", Movie.NEW_RELEASE);
 		Rental rental2 = new Rental(movie2 , 10);
+		Movie movie3 = new Movie("Dory", Movie.CHILDRENS);
+		Rental rental3 = new Rental(movie3 , 2);
 		
 		customer.addRental(rental1);
 		customer.addRental(rental2);
+		customer.addRental(rental3);
 		
-		String expected = "Rental Record for Stefano Frocio\n" +
+		String expected = "Rental Record for Ugo Campione\n" +
 				"\tRitorno al futuro\t6.5\n" +
 				"\tStarWars Episode 8\t30.0\n" +
-				"Amount owed is 36.5\n" +
-				"You earned 3 frequent renter points";
+				"\tDory\t1.5\n" +
+				"Amount owed is 38.0\n" +
+				"You earned 4 frequent renter points";
 		
 		assertEquals(expected, customer.statement());
-		
 	}
 
 }
